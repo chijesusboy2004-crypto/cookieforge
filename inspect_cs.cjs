10a573677090fcd889f5d7697f548f4dc7199f79
CHIBIKO CHIDERA PELUMI <297120147+chijesusboy2004-crypto@users.noreply.github.com>
2026-09-10 08:26:17 +0100

    chore: cleanup inspection scripts

--- inspect_cs.cjs
+++
@@ -1,6 +0,0 @@
-async function check() {
-  const res = await fetch('https://cookiescan.io');
-  const html = await res.text();
-  console.log(html);
-}
-check();
--- test_addr.cjs
+++
@@ -1,5 +0,0 @@
-async function test() {
-  const res = await fetch('https://cookiescan.io/address/2s3TVpCX5FxSJG3FyFXSStVwLwgVNkNukSpgECWUadV5');
-  console.log('HTTP Status:', res.status);
-}
-test();
